3057fa29797cd0f655b77f971ed779f989c28da6
Williano <paawilly17@gmail.com>
2020-02-20 23:54:23 -0600

    Use flutter packages

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,32 +6,8 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-
-  void getData() async{
-    // simulate a network request for a username.
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return "yoshi" ;
-    });
-
-    // simulate a network request to get bio of the username.
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return "vega, muscian, egg collection";
-    });
-
-    print("$username - $bio");
-  }
-
   int counter = 0;
 
-
-  @override
-  void initState() {
-    super.initState();
-
-    getData();
-    print("Hey wait");
-    print("InitState Function run");
-  }
   @override
   Widget build(BuildContext context) {
     print("Build Function run");
