2bf9f1a30245c071afef5557b6da759f081cee92
Williano <paawilly17@gmail.com>
2020-02-22 23:28:04 -0600

    Add custom class for getting data from world time api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,37 +8,34 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  String time = "loading";
 
-  void getTime() async{
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: "Berlin",
+        flag: "flag.png", url: "Europe/Berlin"
+    );
 
-      // Make the request to the time api.
-    Response response = await get("http://worldtimeapi.org/api/timezone/Europe/London");
-    Map data = jsonDecode(response.body);
-//    print(data);
-
-    // get properties from data
-    String dataTime = data["datetime"];
-    String offset = data["utc_offset"].substring(1, 3);
-//    print(dataTime);
-//    print(offset);
-
-    // Create DateTime object.
-    DateTime now = DateTime.parse(dataTime);
-    now = now.add(Duration(hours: int.parse(offset)));
+    await instance.getTime();
     
+    setState(() {
+      time = instance.time;
+    });
+
   }
 
   @override
   void initState() {
     super.initState();
 
-    getTime();
-
+    setupWorldTime();
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Loading screen"),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,34 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+
+class WorldTime {
+  String location; // location name for the UI
+  String time; // the time in that location
+  String flag; // url to an asset flag icon
+  String url; // location url for api endpoint
+
+  WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getTime() async{
+
+    // Make the request to the time api.
+    Response response = await get("http://worldtimeapi.org/api/timezone/$url");
+    Map data = jsonDecode(response.body);
+//    print(data);
+
+    // get properties from data
+    String dataTime = data["datetime"];
+    String offset = data["utc_offset"].substring(1, 3);
+//    print(dataTime);
+//    print(offset);
+
+    // Create DateTime object.
+    DateTime now = DateTime.parse(dataTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    // set the time property
+    time = now.toString();
+
+  }
+}
